23687065b6e7c1affd98004c235079e478354754
Andrew Sykes <andrew.sykes@rabbies.com>
2016-11-03 08:02:04 +0100

    Adding an empty Web Spring Boot project generated by start.spring.io

---
+++ src/main/java/com/rabbies/WebTest1Application.java
@@ -0,0 +1,12 @@
+package com.rabbies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTest1Application.class, args);
+	}
+}
